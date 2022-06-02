4bf1212e64a48a16e621f78367c1bf5c572c7e83
meddwhb <meddw@DESKTOP-DGNC7MK>
2022-06-02 15:06:29 +0100

    Question 7

---
+++ app/src/main/java/com/example/cc2_mohamed_benabdelouhab/List_Entr.java
@@ -0,0 +1,19 @@
+package com.example.cc2_mohamed_benabdelouhab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+
+public class List_Entr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_entr);
+    }
+}
--- app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MainActivity.java
+++ app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.cc2_mohamed_benabdelouhab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void send(View view) {
+        Intent i = null;
+        switch (view.getId()){
+            case R.id.b1: i=new Intent(MainActivity.this, add_entr.class); break;
+            case R.id.b2: i=new Intent(MainActivity.this, Edit_Entr.class); break;
+            case R.id.b3: i=new Intent(MainActivity.this, List_Entr.class); break;
+        }
+        startActivity(i);
     }
 }
