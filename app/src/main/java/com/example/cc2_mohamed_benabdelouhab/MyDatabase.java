79f6b4d0e42912efe4fefa8218852c726b8bd31c
meddwhb <meddw@DESKTOP-DGNC7MK>
2022-06-02 14:20:15 +0100

    Question 4

--- app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MyDatabase.java
+++ app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MyDatabase.java
@@ -8,7 +8,12 @@ import androidx.annotation.Nullable;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
-    public static String DB_NAME="enterprises";
+    public static String DB_NAME="enterprises.db";
+    public static String TABLE_NAME="Entreprise";
+    public static String COL1="ID";
+    public static String COL2="Raison";
+    public static String COL3="adresse";
+    public static String COL4="Capitale";
 
 
     public MyDatabase(@Nullable Context context) {
@@ -17,11 +22,14 @@ public class MyDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String query = "create table " + TABLE_NAME + "("+COL1+" integer primary key autoincrement,"+COL2+" TEXT,"+COL3+" TEXT,"+COL4+" double)";
+        sqLiteDatabase.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        String query= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(query);
+        onCreate(sqLiteDatabase);
     }
 }
