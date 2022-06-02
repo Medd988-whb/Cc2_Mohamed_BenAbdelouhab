d868107b9962ca04f750f3302ad9fde9d36e190f
meddwhb <meddw@DESKTOP-DGNC7MK>
2022-06-02 14:31:53 +0100

    Question 5

--- app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MyDatabase.java
+++ app/src/main/java/com/example/cc2_mohamed_benabdelouhab/MyDatabase.java
@@ -1,10 +1,14 @@
 package com.example.cc2_mohamed_benabdelouhab;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
@@ -31,5 +35,63 @@ public class MyDatabase extends SQLiteOpenHelper {
         String query= "DROP TABLE " + TABLE_NAME;
         sqLiteDatabase.execSQL(query);
         onCreate(sqLiteDatabase);
+    }
+
+
+
+    public static void AddEntreprise(SQLiteDatabase db, Entreprise e){
+        ContentValues cv = new ContentValues();
+        cv.put(COL2,e.getRaison());
+        cv.put(COL3,e.getAdresse());
+        cv.put(COL4,e.getCapitale());
+        db.insert(TABLE_NAME,null,cv);
+    }
+
+
+    public static void UpdateEntreprise(SQLiteDatabase db, Entreprise e){
+        ContentValues cv = new ContentValues();
+        cv.put(COL2,e.getRaison());
+        cv.put(COL3,e.getAdresse());
+        cv.put(COL4,e.getCapitale());
+        db.update(TABLE_NAME,cv,"id="+e.getId(),null);
+    }
+
+
+    public static void DeleteEntreprise(SQLiteDatabase db, int id){
+        db.delete(TABLE_NAME,"id="+id,null);
+    }
+
+
+    public static ArrayList<Entreprise> getAllEntreprise(SQLiteDatabase db){
+        ArrayList<Entreprise> entrs = new ArrayList<>();
+
+        Cursor cur = db.rawQuery("SELECT * FROM " + TABLE_NAME,null);
+
+        while(cur.moveToNext()){
+            Entreprise e = new Entreprise();
+            e.setId(cur.getInt(0));
+            e.setRaison(cur.getString(1));
+            e.setAdresse(cur.getString(2));
+            e.setCapitale(cur.getDouble(3));
+            entrs.add(e);
+        }
+
+        return entrs;
+    }
+
+    public static Entreprise getOneprod(SQLiteDatabase sqLiteDatabase, int id){
+        Entreprise e = null;
+
+        Cursor cur = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE id = " + id,null);
+
+        if(cur.moveToNext()){
+            e = new Entreprise();
+            e.setId(cur.getInt(0));
+            e.setRaison(cur.getString(1));
+            e.setAdresse(cur.getString(2));
+            e.setCapitale(cur.getDouble(3));
+        }
+
+        return e;
     }
 }
